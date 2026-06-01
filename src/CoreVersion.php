e1d5cf60a598411282e9c9ff2c0d3908c9997acd
crystal <crystal@crystalapps.by>
2026-06-01 21:24:32 +0300

    chore: release v1.0.0

--- src/CoreVersion.php
+++ src/CoreVersion.php
@@ -6,7 +6,7 @@ namespace Crystal\Finance\Core;
 
 final readonly class CoreVersion
 {
-    public const string VERSION = '0.9.0';
+    public const string VERSION = '1.0.0';
 
     private function __construct()
     {
